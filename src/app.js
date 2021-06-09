57c6b0798baaf3a99209e4bf70eb4f70cb0f3bd7
Fernando Ordoñez <Fernando.Ordonez@portalesit.net>
2021-06-09 09:57:49 -0500

    palindromo

--- src/app.js
+++ src/app.js
@@ -1,16 +1,13 @@
-const express = require("express");
+const express = require('express');
 const cors = require('cors');
-
 const app = express();
+
 app.use(cors());
 app.use(express.json());
-
-app.use("/", require("./routes/iecho"));
-
+app.use('/', require('./routes/iecho'));
 
 app.listen(3010);
 
+console.log('Server on port', 3010)
 
-console.log("Server on port", 3010);
-
-module.exports = app;
+module.exports = app
--- src/routes/iecho.js
+++ src/routes/iecho.js
@@ -1,24 +1,18 @@
 const { Router } = require('express')
-var cors = require('cors')
 
 const app = Router()
 
-var corsOptions = {
-    origin: 'http://localhost:*',
-    optionsSuccessStatus: 200
+const normalizar = txt => {
+    return txt.trim().replace(/ +/g, '').toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '')
 }
 
 app.get('/iecho', (req, res) => {
-
     if (req.query.text) {
         var _text = req.query.text.split('').reverse().join('')
-        res.json({ text: _text, palindrome: _text.trim().replace(/ +/g, '') == req.query.text.trim().replace(/ +/g, '') })
+        res.json({ text: _text, palindrome: normalizar(_text) == normalizar(req.query.text) })
     }
     else
         res.status(400).json({ error: 'no text' })
 })
-
-
-
 
 module.exports = app
